3accf81a5673c045e1766a18d04559881048fda1
lyon0211 <lyon0211@gmail.com>
2020-09-04 15:19:23 -0700

    Update EnCoder.java

--- src/EnCoder.java
+++ src/EnCoder.java
@@ -1,4 +1,19 @@
+import java.util.*;
 
-public class EnCoder {
-
+public class EnCoder
+{
+	ArrayList < String > dictionary = new ArrayList < String > ();
+	
+	Scanner keyboard = new Scanner ( System.in );
+	
+	while ( ??? )
+	{
+		if ()
+		{
+		}
+		else
+		{
+			
+		}
+	}
 }
