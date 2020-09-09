26a4edbca6db70850aed34eff2d0f36763e6f1c8
lyon0211 <lyon0211@gmail.com>
2020-09-09 10:59:22 -0700

    Update EnCoder.java

--- src/EnCoder.java
+++ src/EnCoder.java
@@ -4,59 +4,61 @@ import java.io.*;
 
 public class EnCoder
 {
-	HashMap < String, Integer > dictionary = new HashMap < String, Integer > ();
-
-	int counter = 1;
-	String previous;
-	String current;
-	String combination;
-
-	FileReader fr = new FileReader ( "message.txt");
-	BufferedReader br = new BufferedReader ( fr );
-
-	previous = null;
-
-	while ( br.ready () )
+	public static void main(String [] args)
 	{
-		current = "" + br.read ();
-		combination = previous + current;
+		HashMap < String, Integer > dictionary = new HashMap < String, Integer > ();
+		String previous;
+		String current;
+		String combination;
+		int counter = 0;
+		String encoded;
 
-		if ( dictionary.containsKey ( combination ) )
+		FileReader fr = new FileReader ( "message.txt");
+		BufferedReader br = new BufferedReader ( fr );
+
+		previous = null;
+
+		while ( br.ready () )
 		{
-			previous = combination;
-		}
-		else if ( combination.length () == 1 )
-		{
-			dictionary.put ( combination, 256 + counter );
-		}
-		else
-		{
-			dictionary.put((previous + current), counter);
-			counter++;
-			encoded += " " + dictionary.get(previous);
-			previous = current;
+			current = "" + br.read ();
+			combination = previous + current;
+
+			if ( dictionary.containsKey ( combination ) )
+			{
+				previous = combination;
+			}
+			else if ( combination.length () == 1 )
+			{
+				dictionary.put ( combination, 256 + counter );
+			}
+			else
+			{
+				dictionary.put((previous + current), counter);
+				counter++;
+				encoded += " " + dictionary.get(previous);
+				previous = current;
+			}
+
+			br.close ();
+			fr.close ();
 		}
 
-		br.close ();
-		fr.close ();
+		catch ( Exception e )
+		{
+			System.out.println ( "Error Occured" );
+		}
+
+		try
+		{
+			FileWriter fw = new FileWriter(new File("encoded.txt"));
+			BufferedWriter buffer = new BufferedWriter(fw);
+			buffer.write(encoded);
+			buffer.close();
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error Occured in Writing.");
+		}
 	}
-	
-	catch ( Exception e )
-	{
-		System.out.println ( "Error Occured" );
-	}
-	
-	try
-	{
-		FileWriter fw = new FileWriter(new File("encoded.txt"));
-		BufferedWriter buffer = new BufferedWriter(fw);
-		buffer.write(encoded);
-		buffer.close();
-	}
-	
-	catch (Exception e)
-	{
-		System.out.println("Error Occured in Writing.");
-	}
-}
 }
